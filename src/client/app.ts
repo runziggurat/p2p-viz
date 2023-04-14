15be3f870a7402c566ca049dd19242f27ba33a3a
kylegranger <kyle.granger@chello.at>
2023-04-14 13:14:30 +0200

    chore: edit filter input label

--- src/client/app.ts
+++ src/client/app.ts
@@ -6,7 +6,7 @@ import { PCamera } from './camera'
 import { EKeyId, IKeyAction } from './core'
 import { zoomLogToScale } from './util'
 
-const APP_VERSION = '0.1.5';
+const APP_VERSION = '0.1.6';
 
 
 export class CApp {
