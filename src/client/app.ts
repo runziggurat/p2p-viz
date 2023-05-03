711e083826d5e268d0d12213ad66b5910532a535
kylegranger <kyle.granger@chello.at>
2023-05-03 09:01:14 +0200

    feat: restore port number to displayed ip address

--- src/client/app.ts
+++ src/client/app.ts
@@ -6,7 +6,7 @@ import { PCamera } from './camera'
 import { EKeyId, IKeyAction } from './core'
 import { zoomLogToScale } from './util'
 
-const APP_VERSION = '0.1.9';
+const APP_VERSION = '0.1.10';
 
 export class CApp {
     private mousekey: CMousekeyCtlr
